d8f2dc665a36bda516e214866227547360941fd8
Alban Bernard <71706996+Boubounech@users.noreply.github.com>
2024-11-11 21:02:15 -0500

    Add controller support

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,25 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject optionsMenu;
 
+    [SerializeField] private Button selectedMainMenuButton;
+    [SerializeField] private Button selectedOptionsMenuButton;
+
     public string firstScene;
+
+    private void Start()
+    {
+        ShowMainMenu();
+    }
 
     public void ShowMainMenu()
     {
         mainMenu.SetActive(true);
         optionsMenu.SetActive(false);
+        selectedMainMenuButton.Select();
     }
 
     public void ShowOptionsMenu()
     {
         mainMenu.SetActive(false);
         optionsMenu.SetActive(true);
+        selectedOptionsMenuButton.Select();
     }
 
     public void CloseGame()
