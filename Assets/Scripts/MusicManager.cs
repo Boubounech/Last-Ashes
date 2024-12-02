6f453cc17446a948e52a8d72f773359537636276
gregmetral <gregmetral02@gmail.com>
2024-12-02 17:13:29 -0500

    ajout musiques

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); 
+            audioSource = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject); 
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Relations_8" || scene.name == "Sleep_12" || scene.name == "MainMenu")
+        {
+            if (audioSource.isPlaying)
+                audioSource.Stop(); 
+        }
+        else
+        {
+            if (!audioSource.isPlaying)
+                audioSource.Play(); 
+        }
+    }
+}
